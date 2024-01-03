ca110f9a0859c6b3ebd106ff6577d849ee727686
FadhilahW28 <2200018228@webmail.uad.ac.id>
2024-01-03 09:44:52 +0700

    03/01/24

--- src/com/ekspedisi/Main.java
+++ src/com/ekspedisi/Main.java
@@ -3,14 +3,36 @@ package com.ekspedisi;
 import com.ekspedisi.ServiceType.Supercepat;
 import com.ekspedisi.ServiceType.Reguler;
 import com.ekspedisi.ServiceType.Ekonomi;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Ekspedisi ekp = new Ekspedisi(null, null, "Jl. Ringroad Selatan, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55191", 5.4, 10.2, 2);
 
-        new Supercepat(ekp);
-        new Reguler(ekp);
-        new Ekonomi(ekp);
+        System.out.println("#  SELAMAT DATANG DI J&T EXPRESS  #");
+        System.out.println("Pilih ServiceType:");
+        System.out.println("1. Supercepat");
+        System.out.println("2. Reguler");
+        System.out.println("3. Ekonomi");
 
+        System.out.println("Masukkan Pilihan Service anda :");
+        Scanner scanner = new Scanner(System.in);
+        int choice = scanner.nextInt();
+
+
+        switch (choice) {
+            case 1:
+                new Supercepat(ekp);
+                break;
+            case 2:
+                new Reguler(ekp);
+                break;
+            case 3:
+                new Ekonomi(ekp);
+                break;
+            default:
+                System.out.println("Pilihan tidak valid");
+        }
     }
 }
