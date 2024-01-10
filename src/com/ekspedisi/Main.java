eff05556426bdd676abb5ce64cd7f89a42f1f414
FadhilahW28 <2200018228@webmail.uad.ac.id>
2024-01-10 10:43:09 +0700

    10/01/24

--- src/com/ekspedisi/Main.java
+++ src/com/ekspedisi/Main.java
@@ -5,34 +5,110 @@ import com.ekspedisi.ServiceType.Reguler;
 import com.ekspedisi.ServiceType.Ekonomi;
 import java.util.Scanner;
 
+import com.ekspedisi.Ekspedisi;
+
 public class Main {
+    public Ekspedisi ekp;
+
     public static void main(String[] args) {
 
-        Ekspedisi ekp = new Ekspedisi(null, null, "Jl. Ringroad Selatan, Kabupaten Bantul, Daerah Istimewa Yogyakarta 55191", 5.4, 10.2, 2);
+
+        Main main = new Main();
 
         System.out.println("#  SELAMAT DATANG DI J&T EXPRESS  #");
+        main.ekp = main.tambah();
         System.out.println("Pilih ServiceType:");
         System.out.println("1. Supercepat");
         System.out.println("2. Reguler");
         System.out.println("3. Ekonomi");
 
-        System.out.println("Masukkan Pilihan Service anda :");
+        System.out.print("Masukkan Pilihan Service anda :");
         Scanner scanner = new Scanner(System.in);
         int choice = scanner.nextInt();
 
-
         switch (choice) {
             case 1:
-                new Supercepat(ekp);
+                new Supercepat(main.ekp);
                 break;
             case 2:
-                new Reguler(ekp);
+                new Reguler(main.ekp);
                 break;
             case 3:
-                new Ekonomi(ekp);
+                new Ekonomi(main.ekp);
                 break;
             default:
                 System.out.println("Pilihan tidak valid");
+        }
+    }
+
+    public Ekspedisi tambah() {
+
+        String alamat;
+        double jarak, berat, ukuran;
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukan Alamat = ");
+        alamat = sc.nextLine();
+        System.out.print("Masukan Jarak = ");
+        jarak = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Masukan Berat = ");
+        berat = sc.nextDouble();
+        sc.nextLine();
+        System.out.print("Masukan Ukuran = ");
+        ukuran = sc.nextDouble();
+        Ekspedisi ekp = new Ekspedisi(null, null,
+                alamat,
+                jarak, berat, ukuran);
+        char confirm;
+        do{
+            System.out.println("Alamat: " + ekp.getAddress());
+            System.out.println("Jarak: " + ekp.getDistance());
+            System.out.println("Berat: " + ekp.getWeight());
+            System.out.println("Ukuran: " + ekp.getSize());
+            System.out.println("Apakah Ada yang mau diedit? (y/n) ");
+            confirm = sc.next().charAt(0);
+            if(confirm == 'y'){
+                edit(ekp);
+            }
+        } while (confirm == 'y');
+
+        return ekp;
+    }
+
+    public void edit(Ekspedisi ekp) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("==================");
+        System.out.println("1.Alamat");
+        System.out.println("2.Jarak");
+        System.out.println("3.Berat");
+        System.out.println("4.Ukuran");
+        System.out.print("Data yang mau diubah (angka saja) = ");
+        int angka = sc.nextInt();
+        sc.nextLine();
+        switch (angka) {
+            case 1:
+                System.out.print("Masukan Alamat = ");
+                String alamat = sc.nextLine();
+                ekp.setAddress(alamat);
+                break;
+            case 2:
+                System.out.print("Masukan Jarak = ");
+                double jarak = sc.nextDouble();
+                ekp.setDistance(jarak);
+                break;
+            case 3:
+                System.out.print("Masukan Berat = ");
+                double berat = sc.nextDouble();
+                ekp.setWeight(berat);
+                break;
+            case 4:
+                System.out.print("Masukan Ukuran = ");
+                double ukuran = sc.nextDouble();
+                ekp.setSize(ukuran);
+                break;
+            default:
+                System.out.println("Pilihan tidak valid.");
+                break;
         }
     }
 }
